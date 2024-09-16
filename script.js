24a13d75e4fb9e08deca699d7e19c0f5b8fc1c07
Jose Ruiz <jruiz793ric@gmail.com>
2024-09-16 15:16:44 -0300

    ButtonJS

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-const lenguajes = document.querySelectorAll('.container .Lenguajes');
+const nameInput = document.querySelector('.name');
+const imgInput = document.querySelector('.urlImg');
+const puestosInput = document.querySelector('.puestosInput');
 const container = document.querySelector('.container');
+const btnSend = document.querySelector('.btn-send');
+
+function checkInputs() {
+    if(nameInput.value && imgInput.value  &&
+         puestosInput.value){
+            btnSend.disabled = false;
+         }else{
+            btnSend.disabled = true;
+         }
+}
+nameInput.addEventListener('input', checkInputs);
+imgInput.addEventListener('input', checkInputs);
+puestosInput.addEventListener('input', checkInputs);
 
 document.querySelector('.btn-send').addEventListener('click', (e) => {
     e.preventDefault();
-
-    console.log('ahora seguiremos desde aquí :)');
 
     if (container.style.display === 'none' || container.style.display === '') {
         container.style.display = 'grid';

bea78dbc15c22f2c63069f1f4d6fa843a19ded29
Jose Ruiz <jruiz793ric@gmail.com>
2024-09-15 23:23:56 -0300

    ButtonJS

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-document.querySelector(`.btn-send`).addEventListener(`click`, (e) => {
-    e.preventDefault()
-    console.log(`mañana seguiremos desde aqui :)`)
-})
+const lenguajes = document.querySelectorAll('.container .Lenguajes');
+const container = document.querySelector('.container');
+
+document.querySelector('.btn-send').addEventListener('click', (e) => {
+    e.preventDefault();
+
+    console.log('ahora seguiremos desde aquí :)');
+
+    if (container.style.display === 'none' || container.style.display === '') {
+        container.style.display = 'grid';
+    } else {
+        container.style.display = 'none';
+    }
+});
